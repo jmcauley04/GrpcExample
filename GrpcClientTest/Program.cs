44ae3a89db67d948480898362d2c03963547bdb3
Michael Cauley <jmcauley04@gmail.com>
2022-12-31 06:55:25 -0500

    Add project files.

---
+++ GrpcClientTest/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GrpcServiceTest;
+
+var channel = Grpc.Net.Client.GrpcChannel.ForAddress("http://localhost:5237");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest() { Name = "Greeter Client Jr" });
+Console.WriteLine($"Reply: {reply.Message}");
+Console.ReadLine();
